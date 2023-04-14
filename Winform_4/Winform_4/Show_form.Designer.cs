b70c8fc694393328b092a81ed44900a21536cdc4
Hyonony <sic6688@naver.com>
2023-04-14 09:09:47 +0900

    use DB show Image

--- Winform_4/Winform_4/Show_form.Designer.cs
+++ Winform_4/Winform_4/Show_form.Designer.cs
@@ -28,25 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.left_button = new System.Windows.Forms.Button();
             this.right_button = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(134, 64);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(536, 376);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
-            // 
             // left_button
             // 
-            this.left_button.Location = new System.Drawing.Point(24, 216);
+            this.left_button.Location = new System.Drawing.Point(18, 266);
             this.left_button.Name = "left_button";
             this.left_button.Size = new System.Drawing.Size(75, 34);
             this.left_button.TabIndex = 1;
@@ -56,7 +49,7 @@
             // 
             // right_button
             // 
-            this.right_button.Location = new System.Drawing.Point(696, 216);
+            this.right_button.Location = new System.Drawing.Point(675, 266);
             this.right_button.Name = "right_button";
             this.right_button.Size = new System.Drawing.Size(75, 34);
             this.right_button.TabIndex = 2;
@@ -64,25 +57,57 @@
             this.right_button.UseVisualStyleBackColor = true;
             this.right_button.Click += new System.EventHandler(this.right_button_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 14F);
+            this.label1.Location = new System.Drawing.Point(315, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 28);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "추천 음식";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.right_button);
+            this.groupBox1.Controls.Add(this.left_button);
+            this.groupBox1.Location = new System.Drawing.Point(13, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(775, 488);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(112, 106);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(536, 364);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
             // Show_form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 500);
-            this.Controls.Add(this.right_button);
-            this.Controls.Add(this.left_button);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox1);
             this.Name = "Show_form";
             this.Text = "Show_form";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.PictureBox pictureBox1;
         public System.Windows.Forms.Button left_button;
         public System.Windows.Forms.Button right_button;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- Winform_4/Winform_4/Show_form.cs
+++ Winform_4/Winform_4/Show_form.cs
@@ -28,7 +28,20 @@ namespace Winform_4
 
         private void right_button_Click(object sender, EventArgs e)
         {
-            
+            string connectionString = "Server=localhost;Database=DB_user;Uid=root;Pwd=root;";
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            connection.Open();
+
+            string query = "SELECT foodImage FROM food WHERE id = 1"; //컬럼(매운, 달달, 짠)
+            MySqlCommand command = new MySqlCommand(query, connection);
+            MySqlDataReader reader = command.ExecuteReader();
+
+            if (reader.Read())
+            {
+                byte[] imageBytes = (byte[])reader["foodImage"];
+                MemoryStream ms = new MemoryStream(imageBytes);
+                pictureBox1.Image = Image.FromStream(ms);
+            }
         }
 
         private void left_button_Click(object sender, EventArgs e)
