fc52ed7ec48f601602ea57be8dcda15f1e74f8c1
Hyonony <sic6688@naver.com>
2023-04-14 11:49:26 +0900

    [Winform_4] use DB show multiple images

--- Winform_4/Winform_4/Show_form.Designer.cs
+++ Winform_4/Winform_4/Show_form.Designer.cs
@@ -28,32 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.left_button = new System.Windows.Forms.Button();
             this.right_button = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblImageId = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // left_button
-            // 
-            this.left_button.Location = new System.Drawing.Point(18, 266);
-            this.left_button.Name = "left_button";
-            this.left_button.Size = new System.Drawing.Size(75, 34);
-            this.left_button.TabIndex = 1;
-            this.left_button.Text = "왼쪽";
-            this.left_button.UseVisualStyleBackColor = true;
-            this.left_button.Click += new System.EventHandler(this.left_button_Click);
-            // 
             // right_button
             // 
-            this.right_button.Location = new System.Drawing.Point(675, 266);
+            this.right_button.Location = new System.Drawing.Point(654, 371);
             this.right_button.Name = "right_button";
-            this.right_button.Size = new System.Drawing.Size(75, 34);
+            this.right_button.Size = new System.Drawing.Size(103, 99);
             this.right_button.TabIndex = 2;
-            this.right_button.Text = "오른쪽";
+            this.right_button.Text = "다음";
             this.right_button.UseVisualStyleBackColor = true;
             this.right_button.Click += new System.EventHandler(this.right_button_Click);
             // 
@@ -61,7 +51,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("굴림", 14F);
-            this.label1.Location = new System.Drawing.Point(315, 53);
+            this.label1.Location = new System.Drawing.Point(109, 73);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(133, 28);
             this.label1.TabIndex = 3;
@@ -69,15 +59,27 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.lblImageId);
             this.groupBox1.Controls.Add(this.pictureBox1);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.right_button);
-            this.groupBox1.Controls.Add(this.left_button);
             this.groupBox1.Location = new System.Drawing.Point(13, 0);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(775, 488);
             this.groupBox1.TabIndex = 4;
             this.groupBox1.TabStop = false;
+            // 
+            // lblImageId
+            // 
+            this.lblImageId.AutoSize = true;
+            this.lblImageId.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblImageId.Font = new System.Drawing.Font("굴림", 14F);
+            this.lblImageId.Location = new System.Drawing.Point(620, 75);
+            this.lblImageId.Name = "lblImageId";
+            this.lblImageId.Size = new System.Drawing.Size(30, 30);
+            this.lblImageId.TabIndex = 5;
+            this.lblImageId.Text = "0";
+            this.lblImageId.Click += new System.EventHandler(this.lblImageId_Click);
             // 
             // pictureBox1
             // 
@@ -95,7 +97,7 @@
             this.ClientSize = new System.Drawing.Size(800, 500);
             this.Controls.Add(this.groupBox1);
             this.Name = "Show_form";
-            this.Text = "Show_form";
+            this.Text = "메뉴 추천";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -104,10 +106,10 @@
         }
 
         #endregion
-        public System.Windows.Forms.Button left_button;
         public System.Windows.Forms.Button right_button;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblImageId;
     }
 }
--- Winform_4/Winform_4/Show_form.cs
+++ Winform_4/Winform_4/Show_form.cs
@@ -15,6 +15,9 @@ namespace Winform_4
 
     public partial class Show_form : Form
     {
+        string connectionString = "Server=localhost;Database=DB_user;Uid=root;Pwd=root;";
+        List<string> imagePaths = new List<string>();
+        string query = "";
 
         public Show_form()
         {
@@ -26,17 +29,18 @@ namespace Winform_4
 
         }
 
+        
+
         private void right_button_Click(object sender, EventArgs e)
         {
-
-            string connectionString = "Server=localhost;Database=DB_user;Uid=root;Pwd=root;";
-            string query = "";
+            
             MySqlConnection connection = new MySqlConnection(connectionString);
             connection.Open();
 
             // 한식
             if (Mainfrom.typecheck == "1")
             {
+                // MessageBox.Show("데이터가 적어서 한식 전체를 보여드리겠습니다.", "알림");
                 // 매운 정도
                 if(level_1.hotlevel == "1")
                 {
@@ -107,6 +111,8 @@ namespace Winform_4
             // 중식
             else if (Mainfrom.typecheck == "2")
             {
+                //MessageBox.Show("데이터가 적어서 중식 전체를 보여드리겠습니다.", "알림");
+
                 // 매운 정도
                 if (level_1.hotlevel == "1")
                 {
@@ -178,6 +184,8 @@ namespace Winform_4
             // 일식
             else if (Mainfrom.typecheck == "3")
             {
+                //MessageBox.Show("데이터가 적어서 일식 전체를 보여드리겠습니다.", "알림");
+
                 if (level_1.hotlevel == "1")
                 {
                     query = "SELECT foodImage FROM food WHERE Food_type = '일식' and spicy = '가장 매운'"; //컬럼(매운, 달달, 짠)
@@ -248,6 +256,8 @@ namespace Winform_4
             // 양식
             else if (Mainfrom.typecheck == "4")
             {
+                //MessageBox.Show("데이터가 적어서 양식 전체를 보여드리겠습니다.", "알림");
+
                 if (level_1.hotlevel == "1")
                 {
                     query = "SELECT foodImage FROM food WHERE Food_type = '양식' and spicy = '가장 매운'"; //컬럼(매운, 달달, 짠)
@@ -318,6 +328,8 @@ namespace Winform_4
             // 기타
             else if (Mainfrom.typecheck == "5")
             {
+                // MessageBox.Show("데이터가 적어서 기타 전체를 보여드리겠습니다.", "알림");
+
                 if (level_1.hotlevel == "1")
                 {
                     query = "SELECT foodImage FROM food WHERE Food_type = '기타' and spicy = '가장 매운'"; //컬럼(매운, 달달, 짠)
@@ -384,25 +396,92 @@ namespace Winform_4
                 }
 
             }
+            /*
+            MySqlCommand command = new MySqlCommand(query, connection);
+            using (MySqlDataReader reader = command.ExecuteReader())
+            {
+                if (reader.Read())
+                {
+                    byte[] imageBytes = (byte[])reader["foodImage"];
+                    using (MemoryStream ms = new MemoryStream(imageBytes))
+                    {
+                        pictureBox1.Image = Image.FromStream(ms);
+                    }
+                }
+            }
+            */
+            int currentImageId = int.Parse(lblImageId.Text);
+
+            int nextImageId = currentImageId + 1;
+
+            if (CheckIfImageExists(nextImageId))
+            {
+                // 다음 이미지가 존재하면 해당 이미지를 보여줍니다.
+                ShowImageInPictureBox(nextImageId);
+            }
+            else
+            {
+                // 다음 이미지가 존재하지 않으면 첫 번째 이미지를 보여줍니다.
+                ShowImageInPictureBox(1);
+            }
+        }
+        private bool CheckIfImageExists(int imageId)
+        {
+            string connectionString = "Server=localhost;Database=DB_user;Uid=root;Pwd=root;";
+            MySqlConnection connection = new MySqlConnection(connectionString);
+
+            string query = $"SELECT COUNT(*) FROM food WHERE id = {imageId}";
 
             MySqlCommand command = new MySqlCommand(query, connection);
-            MySqlDataReader reader = command.ExecuteReader();
+            connection.Open();
 
+            object result = null;
+            if (command != null)
+            {
+                result = command.ExecuteScalar();
+            }
+            int count = result == null ? 0 : Convert.ToInt32(result);
+
+            connection.Close();
+
+            return (count > 0);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // 첫 번째 이미지를 보여줍니다.
+            ShowImageInPictureBox(1);
+
+            // 이미지 ID값을 보여주는 Label 컨트롤에 1을 설정합니다.
+            lblImageId.Text = "1";
+        }
+        private void ShowImageInPictureBox(int imageId)
+        {
+            string connectionString = "Server=localhost;Database=DB_user;Uid=root;Pwd=root;";
+            string query = $"SELECT * FROM food WHERE id = {imageId}";
+
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            MySqlCommand command = new MySqlCommand(query, connection);
+            connection.Open();
+        
+            MySqlDataReader reader = command.ExecuteReader();
             if (reader.Read())
             {
                 byte[] imageBytes = (byte[])reader["foodImage"];
                 MemoryStream ms = new MemoryStream(imageBytes);
                 pictureBox1.Image = Image.FromStream(ms);
+
+                // 현재 보여지고 있는 이미지의 ID값을 설정합니다.
+                lblImageId.Text = imageId.ToString();
             }
+
+            connection.Close();
+            reader.Close();
         }
 
-        private void left_button_Click(object sender, EventArgs e)
-        {
-            //MessageBox.Show(Mainfrom.typecheck);
-            //MessageBox.Show(level_1.hotlevel);
-            //MessageBox.Show(level_2.sweetlevel);
-            //MessageBox.Show(level_3.saltlevel);
+        
 
+        private void lblImageId_Click(object sender, EventArgs e)
+        {
 
         }
     }
