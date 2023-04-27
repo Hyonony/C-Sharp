280ad1f46c286ca0f0c0fe33ba897f104f3e7e28
hyeoneon.oh <hyeoneon.oh@techwing.co.kr>
2023-04-27 10:12:27 +0900

    Winform_5 baseball game v0.1

--- Winform_5/Winform_5/Mainform.Designer.cs
+++ Winform_5/Winform_5/Mainform.Designer.cs
@@ -28,20 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.Throw_Button = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // Form1
+            // Throw_Button
+            // 
+            this.Throw_Button.Location = new System.Drawing.Point(388, 95);
+            this.Throw_Button.Name = "Throw_Button";
+            this.Throw_Button.Size = new System.Drawing.Size(98, 28);
+            this.Throw_Button.TabIndex = 0;
+            this.Throw_Button.Text = "Throw!";
+            this.Throw_Button.UseVisualStyleBackColor = true;
+            this.Throw_Button.Click += new System.EventHandler(this.Throw_Button_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 11F);
+            this.label1.Location = new System.Drawing.Point(107, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(379, 22);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "서로 다른 3자리 숫자를 입력해주세요";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(111, 95);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(263, 28);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.textBox1_PreviewKeyDown);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 18;
+            this.listBox1.Location = new System.Drawing.Point(89, 154);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(424, 382);
+            this.listBox1.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.Throw_Button);
+            this.groupBox1.Location = new System.Drawing.Point(65, 54);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(588, 602);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            // 
+            // Mainform
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(700, 700);
-            this.Name = "MainForm";
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Mainform";
             this.Text = "메인 화면";
+            this.Load += new System.EventHandler(this.Mainform_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Throw_Button;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
 
