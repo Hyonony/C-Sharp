b9e8d76311395c4e773fa5a5e64c2c3ff971e3f8
hyeoneon.oh <hyeoneon.oh@techwing.co.kr>
2023-04-27 10:29:40 +0900

    fix winform_5 step

---
+++ Winform_5/Winform_5/BaseballGame.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Winform_5
+{
+    partial class BaseBallGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Throw_Button = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.Throw_Button);
+            this.groupBox1.Location = new System.Drawing.Point(56, 49);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(588, 602);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 18;
+            this.listBox1.Location = new System.Drawing.Point(89, 154);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(424, 382);
+            this.listBox1.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(111, 95);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(263, 28);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 11F);
+            this.label1.Location = new System.Drawing.Point(107, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(379, 22);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "서로 다른 3자리 숫자를 입력해주세요";
+            // 
+            // Throw_Button
+            // 
+            this.Throw_Button.Location = new System.Drawing.Point(388, 95);
+            this.Throw_Button.Name = "Throw_Button";
+            this.Throw_Button.Size = new System.Drawing.Size(98, 28);
+            this.Throw_Button.TabIndex = 0;
+            this.Throw_Button.Text = "Throw!";
+            this.Throw_Button.UseVisualStyleBackColor = true;
+            this.Throw_Button.Click += new System.EventHandler(this.Throw_Button_Click);
+            // 
+            // BaseballGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(700, 700);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "BaseballGame";
+            this.Text = "BaseballGame";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Throw_Button;
+    }
+}
---
+++ Winform_5/Winform_5/BaseballGame.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Winform_5
+{
+    public partial class BaseBallGame : Form
+    {
+        private int[] answer = new int[3]; // 3자리 숫자
+        private int count = 0; // 시도 횟수
+
+        public BaseBallGame()
+        {
+            InitializeComponent();
+            GenerateAnswer(); // 정답 생성
+        }
+        private void GenerateAnswer()
+        {
+            // 중복되지 않는 0~9의 숫자로 이루어진 3자리 숫자 생성
+            Random random = new Random();
+            for (int i = 0; i < 3; i++)
+            {
+                int num;
+                do
+                {
+                    num = random.Next(0, 10);
+                } while (Array.IndexOf(answer, num) != -1);
+                answer[i] = num;
+
+            }
+        }
+
+        private void Throw_Button_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length != 3)
+            {
+                MessageBox.Show("3자리 숫자를 입력하세요.");
+                return;
+            }
+
+            int[] input = new int[3];
+            for (int i = 0; i < 3; i++)
+            {
+                input[i] = int.Parse(textBox1.Text[i].ToString());
+            }
+
+            int ball = 0;
+            int strike = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                if (input[i] == answer[i])
+                {
+                    strike++;
+                }
+                else if (Array.IndexOf(answer, input[i]) != -1)
+                {
+                    ball++;
+                }
+            }
+
+            count++;
+            listBox1.Items.Add(string.Format("[{0}] {1}, {2}B {3}S", count, textBox1.Text, ball, strike));
+
+            if (strike == 3)
+            {
+                MessageBox.Show("축하합니다. 정답을 맞췄습니다.");
+                GenerateAnswer(); // 새로운 문제 출제
+                count = 0; // 시도 횟수 초기화
+                listBox1.Items.Clear(); // 리스트 박스 초기화
+            }
+
+            textBox1.Clear();
+            textBox1.Focus();
+        }
+
+        private void Mainform_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                // Enter 키를 눌렀을 때 실행할 코드 작성
+                if (textBox1.Text.Length != 3)
+                {
+                    MessageBox.Show("3자리 숫자를 입력하세요.");
+                    return;
+                }
+
+                int[] input = new int[3];
+                for (int i = 0; i < 3; i++)
+                {
+                    input[i] = int.Parse(textBox1.Text[i].ToString());
+                }
+
+                int ball = 0;
+                int strike = 0;
+                for (int i = 0; i < 3; i++)
+                {
+                    if (input[i] == answer[i])
+                    {
+                        strike++;
+                    }
+                    else if (Array.IndexOf(answer, input[i]) != -1)
+                    {
+                        ball++;
+                    }
+                }
+
+                count++;
+                listBox1.Items.Add(string.Format("[{0}] {1}, {2}B {3}S", count, textBox1.Text, ball, strike));
+
+                if (strike == 3)
+                {
+                    MessageBox.Show("축하합니다. 정답을 맞췄습니다. 새로운 문제로 넘어갑니다 !");
+                    GenerateAnswer(); // 새로운 문제 출제
+                    count = 0; // 시도 횟수 초기화
+                    listBox1.Items.Clear(); // 리스트 박스 초기화
+                }
+
+                textBox1.Clear();
+                textBox1.Focus();
+            }
+        }
+    }
+}
