cc8dfeca59d845d6a6d7d197b352e46afb5b3f0f
Hyonony <sic6688@naver.com>
2023-06-19 09:45:31 +0900

    Game Form Add

--- Winform_8/Winform_8/GameForm.Designer.cs
+++ Winform_8/Winform_8/GameForm.Designer.cs
@@ -28,19 +28,87 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GameForm));
+            this.StartButton = new System.Windows.Forms.Button();
+            this.ScoreLabel = new System.Windows.Forms.Label();
+            this.GamePanel = new System.Windows.Forms.Panel();
+            this.Ball = new System.Windows.Forms.PictureBox();
+            this.Target = new System.Windows.Forms.PictureBox();
+            this.GamePanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Target)).BeginInit();
             this.SuspendLayout();
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(77, 86);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(96, 33);
+            this.StartButton.TabIndex = 0;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            // 
+            // ScoreLabel
+            // 
+            this.ScoreLabel.AutoSize = true;
+            this.ScoreLabel.Location = new System.Drawing.Point(615, 93);
+            this.ScoreLabel.Name = "ScoreLabel";
+            this.ScoreLabel.Size = new System.Drawing.Size(68, 18);
+            this.ScoreLabel.TabIndex = 1;
+            this.ScoreLabel.Text = "          ";
+            this.ScoreLabel.Click += new System.EventHandler(this.ScoreLabel_Click);
+            // 
+            // GamePanel
+            // 
+            this.GamePanel.Controls.Add(this.Target);
+            this.GamePanel.Controls.Add(this.Ball);
+            this.GamePanel.Location = new System.Drawing.Point(77, 137);
+            this.GamePanel.Name = "GamePanel";
+            this.GamePanel.Size = new System.Drawing.Size(648, 581);
+            this.GamePanel.TabIndex = 2;
+            // 
+            // Ball
+            // 
+            this.Ball.Image = ((System.Drawing.Image)(resources.GetObject("Ball.Image")));
+            this.Ball.Location = new System.Drawing.Point(63, 343);
+            this.Ball.Name = "Ball";
+            this.Ball.Size = new System.Drawing.Size(96, 73);
+            this.Ball.TabIndex = 0;
+            this.Ball.TabStop = false;
+            // 
+            // Target
+            // 
+            this.Target.Image = ((System.Drawing.Image)(resources.GetObject("Target.Image")));
+            this.Target.Location = new System.Drawing.Point(199, 343);
+            this.Target.Name = "Target";
+            this.Target.Size = new System.Drawing.Size(96, 73);
+            this.Target.TabIndex = 1;
+            this.Target.TabStop = false;
             // 
             // GameForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 800);
+            this.Controls.Add(this.GamePanel);
+            this.Controls.Add(this.ScoreLabel);
+            this.Controls.Add(this.StartButton);
             this.Name = "GameForm";
             this.Text = "게임 화면";
+            this.GamePanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Target)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Label ScoreLabel;
+        private System.Windows.Forms.Panel GamePanel;
+        private System.Windows.Forms.PictureBox Target;
+        private System.Windows.Forms.PictureBox Ball;
     }
 }
--- Winform_8/Winform_8/GameForm.cs
+++ Winform_8/Winform_8/GameForm.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using ThreadWinFormExample;
@@ -13,6 +9,9 @@ namespace Winform_8
 {
     public partial class GameForm : Form
     {
+        private int score = 0;
+        private CancellationTokenSource cts;
+
         private MainForm mainForm;
 
         public GameForm(MainForm mainForm)
@@ -20,5 +19,58 @@ namespace Winform_8
             InitializeComponent();
             this.mainForm = mainForm;
         }
+
+        private void ScoreLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            if (cts != null)
+            {
+                cts.Cancel();
+                cts.Dispose();
+            }
+
+            cts = new CancellationTokenSource();
+            Task.Run(() => MoveTarget(cts.Token));
+        }
+        private async Task MoveTarget(CancellationToken cancellationToken)
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                await Task.Delay(50);
+                Invoke(new Action(() =>
+                {
+                    Target.Left += 5;
+                    if (Target.Right > GamePanel.Width || Target.Left < 0)
+                    {
+                        Target.Left -= 10;
+                    }
+
+                    // Check for a collision with the Ball
+                    if (Ball.Bounds.IntersectsWith(Target.Bounds))
+                    {
+                        score++;
+                        ScoreLabel.Text = "Score: " + score;
+                        Ball.Location = new Point(0, 0);
+                    }
+                }));
+            }
+        }
+        private async void MainForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                Ball.Location = new Point(0, GamePanel.Height - Ball.Size.Height);
+                while (Ball.Top > 0)
+                {
+                    await Task.Delay(50); // pause for 50 milliseconds
+                    Invoke(new Action(() => { Ball.Top -= 10; })); // move the ball
+                }
+                Ball.Location = new Point(0, GamePanel.Height - Ball.Size.Height); // reset the ball
+            }
+        }
+
     }
 }
